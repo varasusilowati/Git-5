953cbe396d033fb4bebafda30927f4bea18c4387
varasusilowati <varasusi05@gmail.com>
2026-01-31 11:54:36 +0700

    Upload tugas sesi 3 sampai sesi 5

---
+++ Sesi 3/sesi3_vara.js
@@ -0,0 +1,7 @@
+for (let i = 1; i <= 4; i++) {
+  let bintang = "";
+  for (let j = 1; j <= i; j++) {
+    bintang += "*";
+  }
+  console.log(bintang);
+}
---
+++ Sesi 4/rumus.js
@@ -0,0 +1,18 @@
+// rumus.js
+export function kalkulator(angka1, angka2, operator) {
+  switch (operator) {
+    case "+":
+      return angka1 + angka2;
+    case "-":
+      return angka1 - angka2;
+    case "*":
+      return angka1 * angka2;
+    case "/":
+      if (angka2 === 0) {
+        return "Tidak bisa dibagi 0!";
+      }
+      return angka1 / angka2;
+    default:
+      return "Operator tidak valid!";
+  }
+}
